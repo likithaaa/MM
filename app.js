59d7546a8d8f91a2568be6d2617be3d96372b07c
Likitha Duggirala <likithaduggirala@likithas-air.attlocal.net>
2020-06-22 19:42:54 -0400

    updated CSS and JS

--- app.js
+++ app.js
@@ -1,74 +1,86 @@
-const recipesURL = "https://api.spoonacular.com/recipes/findByIngredients";
-const apiKey = "?apiKey=9d03d0ace13f40a994d4da7726fc8c77";
-const queryType = "ingredients=";
-let titleQuery = " ";
+const recipesURL = "https://tasty.p.rapidapi.com/recipes/list?from=0&sizes=10";
+const apiKey = "70ceef3ddcmsh88831d09e9e8948p113056jsn30d2370e5888";
+const queryType = "q=";
+let titleQuery = " "
 const queryURL = recipesURL + apiKey + "&" + queryType;
+
 let getRecipe = () => {
-    let recipes = $.ajax({
+    let result = $.ajax({
         type: 'GET',
-        async: true,
-        url: queryURL + titleQuery
-    })  .then((recipes) => {
-        console.log("These are recipes")
-        console.log(recipes)
-        if (recipes.length > 0) {
-            recipes.push(recipes);
+        crossDomain: true,
+        async: true,        
+        url: recipesURL + "&" + queryType + titleQuery,
+        headers: {
+            "x-rapidapi-host": "tasty.p.rapidapi.com",
+            "x-rapidapi-key": "70ceef3ddcmsh88831d09e9e8948p113056jsn30d2370e5888"
+            }
+    }) .then((result) => {
+       console.log("These are recipes")
+       console.log(result)
+       let results = result.results;
+       //console.log(results[1].instructions[0].display_text)
+
+        if (results.length > 0) {
             $("#input-box").val(" "); 
-            for (let i = 0; i < recipes.length; i++) {
-                $('.container').html(`
-                <h3> ${recipes[0].title} </h3>
-                <img src="${recipes[0].image}">
-                <h4> ${recipes[0].missedIngredients[0].originalString} </h4>
-                <h4> ${recipes[0].missedIngredients[1].originalString} </h4>
-                <h4> ${recipes[0].missedIngredients[2].originalString} </h4>
-                `)
-
-                $('.container2').html(`
-                <h3> ${recipes[1].title} </h3>
-                <img src="${recipes[1].image}">
-                <h4> ${recipes[1].missedIngredients[0].originalString} </h4>
-                <h4> ${recipes[0].missedIngredients[1].originalString} </h4>
-                <h4> ${recipes[0].missedIngredients[2].originalString} </h4>
-                `)
-
-                $('.container3').html(`
-                <h3> ${recipes[2].title} </h3>
-                <img src="${recipes[2].image}">
-                <h4> ${recipes[2].missedIngredients[0].originalString} </h4>
-                <h4> ${recipes[2].missedIngredients[1].originalString} </h4>
-                <h4> ${recipes[2].missedIngredients[2].originalString} </h4>
-                `)
-
-                $('.container4').html(`
-                <h3> ${recipes[3].title} </h3>
-                <img src="${recipes[3].image}">
-                <h4> ${recipes[3].missedIngredients[0].originalString} </h4>
-                <h4> ${recipes[3].missedIngredients[1].originalString} </h4>
-                <h4> ${recipes[3].missedIngredients[2].originalString} </h4>
-                `)
-
-                $('.container5').html(`
-                <h3> ${recipes[4].title} </h3>
-                <img src="${recipes[4].image}">
-                <h4> ${recipes[4].missedIngredients[0].originalString} </h4>
-                <h4> ${recipes[4].missedIngredients[1].originalString} </h4>
-                <h4> ${recipes[4].missedIngredients[2].originalString} </h4> 
-                `)
-                
-            //getRecipe()
-            } return recipes.responseJSON
-        } 
-       });
-       console.log(recipes)
-    //console.log(recipes.responseJSON)
-    // let recipes = $.ajax({ 
-    //     url: queryURL + titleQuery
+            for (let i = 0; i < 10; i++) {
+                $('#container').append(`
+                <div id="box">
+                <h3> ${results[i].name} </h3>
+                <img src="${results[i].thumbnail_url}" width=350px, height= 200px>
+                </div>
+                `) 
+                for (let y = 0; y < 10; y++) {
+                    let recipeIn = results[i].instructions[y].display_text
+                    $('#box').append(`
+                    <p> ${recipeIn} </p>
+                    `) 
+                } 
+            }
+        }
+    })
 }
 
 $(() => {  
-$("#submit").on("click", (event) => {
-    const value = $("#input-box").val();
-    titleQuery = value;
-    getRecipe()
+    $("#submit").on("click", (event) => {
+        const value = $("#input-box").val();
+        titleQuery = value;
+        getRecipe()
+        })
     })
-})
+
+
+    // let getRecipe = () => {
+    //     let result = $.ajax({
+    //         type: 'GET',
+    //         crossDomain: true,
+    //         async: true,        
+    //         url: recipesURL + "&" + queryType + titleQuery,
+    //         headers: {
+    //             "x-rapidapi-host": "tasty.p.rapidapi.com",
+    //             "x-rapidapi-key": "70ceef3ddcmsh88831d09e9e8948p113056jsn30d2370e5888"
+    //             }
+    //     }) .then((result) => {
+    //        console.log("These are recipes")
+    //        console.log(result)
+    //        let results = result.results;
+    //        console.log(results[1].instructions[0].display_text)
+    
+    //         if (results.length > 0) {
+    //             $("#input-box").val(" "); 
+    //             for (let i = 0; i < 10; i++)             $('#container').append(`
+    //                 <h3> ${results[i].name} </h3>
+    //                 <img src="${results[i].thumbnail_url}" width=350px, height= 200px>
+    //         `)
+    //             } 
+    //             for (let y = 0; y < results.length; y++) {
+    //          let recipeIn = results[y].instructions[y].display_text
+    //             }
+
+    
+    // $(() => {  
+    //     $("#submit").on("click", (event) => {
+    //         const value = $("#input-box").val();
+    //         titleQuery = value;
+    //         getRecipe()
+    //         })
+    //     })
