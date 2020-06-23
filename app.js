05c8bd493cf7a28141a086c325b7b4efad302f8b
Likitha Duggirala <likithaduggirala@likithas-air.attlocal.net>
2020-06-23 00:27:02 -0400

    finishing touches

--- app.js
+++ app.js
@@ -22,18 +22,19 @@ let getRecipe = () => {
 
         if (results.length > 0) {
             $("#input-box").val(" "); 
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < 5; i++) {
                 $('#container').append(`
-                <div id="box">
+                <div id='box${i}'>
                 <h3> ${results[i].name} </h3>
                 <img src="${results[i].thumbnail_url}" width=350px, height= 200px>
                 </div>
                 `) 
-                for (let y = 0; y < 10; y++) {
+                for (let y = 0; y < results[i].instructions.length; y++) {
                     let recipeIn = results[i].instructions[y].display_text
-                    $('#box').append(`
+                    $(`#box${i}`).append(`
                     <p> ${recipeIn} </p>
                     `) 
+                    
                 } 
             }
         }
@@ -48,39 +49,3 @@ $(() => {
         })
     })
 
-
-    // let getRecipe = () => {
-    //     let result = $.ajax({
-    //         type: 'GET',
-    //         crossDomain: true,
-    //         async: true,        
-    //         url: recipesURL + "&" + queryType + titleQuery,
-    //         headers: {
-    //             "x-rapidapi-host": "tasty.p.rapidapi.com",
-    //             "x-rapidapi-key": "70ceef3ddcmsh88831d09e9e8948p113056jsn30d2370e5888"
-    //             }
-    //     }) .then((result) => {
-    //        console.log("These are recipes")
-    //        console.log(result)
-    //        let results = result.results;
-    //        console.log(results[1].instructions[0].display_text)
-    
-    //         if (results.length > 0) {
-    //             $("#input-box").val(" "); 
-    //             for (let i = 0; i < 10; i++)             $('#container').append(`
-    //                 <h3> ${results[i].name} </h3>
-    //                 <img src="${results[i].thumbnail_url}" width=350px, height= 200px>
-    //         `)
-    //             } 
-    //             for (let y = 0; y < results.length; y++) {
-    //          let recipeIn = results[y].instructions[y].display_text
-    //             }
-
-    
-    // $(() => {  
-    //     $("#submit").on("click", (event) => {
-    //         const value = $("#input-box").val();
-    //         titleQuery = value;
-    //         getRecipe()
-    //         })
-    //     })
